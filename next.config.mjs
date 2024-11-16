7b29823b2b97fd00a2b46b54cf9ccb0a8a0dad50
Duverney muriel <duverwild@gmail.com>
2024-11-16 11:56:56 +0100

    sentry out

--- next.config.mjs
+++ next.config.mjs
@@ -8,3 +8,15 @@ const nextConfig = {
 };
 
 
+export default {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // Evita que Sentry se incluya en el bundle del cliente
+            config.externals.push({
+                '@sentry/node': 'commonjs @sentry/node',
+                '@sentry/tracing': 'commonjs @sentry/tracing',
+            });
+        }
+        return config;
+    },
+};
